7e557a9bfe0e521966439640146618b14f0fe5ab
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-02-05 17:55:11 -0500

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(port);
+
   console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
